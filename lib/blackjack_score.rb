0140cfad67c9a984604bc4f406d80c29e7fac21c
qqdipps <sdipp007@gmail.com>
2019-02-20 13:56:29 -0800

    Added Blakjack scoring logic

--- lib/blackjack_score.rb
+++ lib/blackjack_score.rb
@@ -1,7 +1,17 @@
 # blackjack_score.rb
 
-VALID_CARDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'King', 'Queen', 'Jack']
+VALID_CARDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "King", "Queen", "Jack"]
 
 def blackjack_score(hand)
-
+  #facecards
+  hand.map! do |card|
+    card == "King" || card == "Queen" || card == "Jack" ? 10 : card
+  end
+  #sum
+  score = hand.inject(:+)
+  #ace
+  if hand.include?(1) && score + 10 <= 21
+    score += 10
+  end
+  score
 end
